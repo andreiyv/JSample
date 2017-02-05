5542966fdf13fb6449aee13a21f8731ccded7698
Andrey <voran@inbox.ru>
2017-02-05 19:56:55 +0300

    ArrayListDemo beta version

--- src/main/java/ArrayListDemo.java
+++ src/main/java/ArrayListDemo.java
@@ -1,7 +1,27 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 class ArrayListDemo{
-    
-    public ArrayListDemo(){
-    
+
+    public static void main(String[] args){
+        ArrayList<Integer> lst = new ArrayList<Integer>();;
+        int sum = 0;
+        int sum1 = 0;
+
+        for (int i=0; i < 1000; i++){
+            lst.add(i);
+        }
+
+        for (int n: lst){
+            sum = sum + n;
+        }
+
+        for (int n=0; n < lst.size(); n++ ){
+          sum1 = sum1 + lst.get(n);
+        }
+
+        System.out.println("ArrayListDemo, summ of all elements:" + sum + ", " + sum1);
     }
 
 }
