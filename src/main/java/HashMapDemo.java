208b9ba546076cb66de9aebc0253bbed39da1c07
Andrey Voronin <andrey.voronin@cd-adapco.com>
2017-02-07 09:05:37 +0300

    HashMApDemo

---
+++ src/main/java/HashMapDemo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class HashMapDemo {
+    
+    public static void main (String[] args) {
+        HashMap hm = new HashMap();
+
+        hm.put("a", new Integer(1));    
+
+        System.out.println("HashMapDemo");
+    }
+
+
+}
+
